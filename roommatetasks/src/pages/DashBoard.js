10e8cf6cd216edc9a345ff3092ca56df619da8e6
dannyceron94 <dannyceron94@gmail.com>
2019-11-29 06:16:49 -0800

    update to task css

--- roommatetasks/src/pages/DashBoard.js
+++ roommatetasks/src/pages/DashBoard.js
@@ -9,18 +9,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const DashBoard = () => {
 
     const [task, setTask] = React.useState('');
-    
+
 
     const createTask = () => {
         const body = {
-            note:task,
-            user:"maybe add user"
+            note: task,
+            user: "maybe add user"
         }
         axios.post('/endpoint', body)
             .then((res) => {
                 console.log(res);
             })
             .catch(console.log);
+    }
+    const changeStatus = (validate) => {
+        // some axios call to chage status to done or not done
+        if (validate === true) {
+            console.log('set status to done');
+        } else {
+            console.log('set status to not done');
+        }
+    }
+
+    const deleteNote=(noteID)=>{
+        console.log(noteID+" will be deleted")
+
     }
 
     return (
@@ -29,59 +42,68 @@ const DashBoard = () => {
             <div className="outterBox">
                 <div className="innerBox">
 
-                    <div className="noteBox">
+                <div className="noteBox">
                         <div className="row">
-                            <div className="col"> static sanoke notes
-                            <input className="check" type="checkbox"></input>
+                            <div className="col-1"><input className="check" type="checkbox"></input></div>
+                            <div className="col-8">  notesstatic sanoke notes
                             </div>
+                            <div className="col-1"><button className="deleteBot" onClick={() =>deleteNote('some id')}>x</button></div>
                         </div>
                     </div>
                     <div className="noteBox">
                         <div className="row">
-                            <div className="col"> static sanoke notes
-                            <input className="check" type="checkbox"></input>
+                            <div className="col-1"><input className="check" type="checkbox"></input></div>
+                            <div className="col-8">  notesstatic sanoke notes
                             </div>
+                            <div className="col-1"><button className="deleteBot" onClick={() =>deleteNote('some id')}>x</button></div>
                         </div>
                     </div>
                     <div className="noteBox">
                         <div className="row">
-                            <div className="col"> static sanoke notes
-                            <input className="check" type="checkbox"></input>
+                            <div className="col-1"><input className="check" type="checkbox"></input></div>
+                            <div className="col-8">  notesstatic sanoke notes
                             </div>
+                            <div className="col-1"><button className="deleteBot" onClick={() =>deleteNote('some id')}>x</button></div>
                         </div>
                     </div>
                     <div className="noteBox">
                         <div className="row">
-                            <div className="col"> static sanoke notes
-                            <input className="check" type="checkbox"></input>
+                            <div className="col-1"><input className="check" type="checkbox"></input></div>
+                            <div className="col-8">  notesstatic sanoke notes
                             </div>
+                            <div className="col-1"><button className="deleteBot" onClick={() =>deleteNote('some id')}>x</button></div>
                         </div>
                     </div>
                     <div className="noteBox">
                         <div className="row">
-                            <div className="col"> static sanoke notes
-                            <input className="check" type="checkbox"></input>
+                            <div className="col-1"><input className="check" type="checkbox"></input></div>
+                            <div className="col-8"> static sanoke notesstatic sanoke notesstatic sanoke notesstatic sanoke notesstatic sanoke notesstatic sanoke notes
                             </div>
+                            <div className="col-1"><button className="deleteBot" onClick={() =>deleteNote('some id')}>x</button></div>
                         </div>
                     </div>
+
                     <div className="noteBox">
                         <div className="row">
-                            <div className="col"> static sanoke notes
-                            <input className="check" type="checkbox"></input>
+                            <div className="col-1"><input className="check" type="checkbox"></input></div>
+                            <div className="col-8"> static sanoke notesstatic sanoke notesstatic sanoke notesstatic sanoke notesstatic sanoke notesstatic sanoke notes
                             </div>
+                            <div className="col-1"><button className="deleteBot" onClick={() =>deleteNote('some id')}>x</button></div>
                         </div>
                     </div>
-                    <div className="noteBox">
+
+                    <div className="noteBoxDone">
                         <div className="row">
-                            <div className="col"> static sanoke notes 
-                            <input className="check" type="checkbox"></input>
+                            <div className="col-1"><input className="check" type="checkbox" onChange={e => { changeStatus(e.target.checked) }}></input></div>
+                            <div className="col-8"> static sanoke notes
                             </div>
+                            <div className="col-1"><button className="deleteBot" onClick={() =>deleteNote('some id')}>x</button></div>
                         </div>
                     </div>
 
                 </div>
                 <div className="marginBot" >
-                    <input className="inputsz" value={task} onChange={e=>{setTask(e.target.value)}}/>
+                    <input className="inputsz" value={task} onChange={e => { setTask(e.target.value) }} />
                     <button className="inputsz" onClick={createTask}>Add Task</button>
                 </div>
             </div>
